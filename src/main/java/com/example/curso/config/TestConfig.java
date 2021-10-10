b4549f9c91364b509ef7062ea0d1fab4a9ab1b9c
Matheus <matheusportinho12@gmail.com>
2021-10-10 05:28:34 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.example.curso.config;
 
 import com.example.curso.entite.Category;
 import com.example.curso.entite.Order;
+import com.example.curso.entite.Product;
 import com.example.curso.entite.User;
 import com.example.curso.entite.enums.OrderStatus;
 import com.example.curso.repositories.CategoryRepository;
 import com.example.curso.repositories.OrderRepository;
+import com.example.curso.repositories.ProductRepository;
 import com.example.curso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,12 +30,21 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         Category cat1 = new Category(null, "Eletronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Phone");
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "3146846415", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "68453434866", "123456");
@@ -42,8 +53,19 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2021-11-10T03:42:45Z"), OrderStatus.WAITING_PAYMENT,u2);
         Order o3 = new Order(null, Instant.parse("2021-11-11T15:21:25Z"), OrderStatus.WAITING_PAYMENT,u1);
 
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
     }
 }
--- src/main/java/com/example/curso/entite/Category.java
+++ src/main/java/com/example/curso/entite/Category.java
@@ -1,8 +1,13 @@
 package com.example.curso.entite;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +18,11 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
 
@@ -37,6 +47,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
